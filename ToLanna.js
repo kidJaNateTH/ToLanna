3cdbe8a5acaecda7f25db7662f0c5d660b463c30
NateTH <64061240+kidJaNateTH@users.noreply.github.com>
2021-06-02 10:26:23 +0700

    ฮองฮับการใข้งานบน NodeJS สำหรับคนตี่เขียน NodeJS

---
+++ ToLanna.js
@@ -0,0 +1,41 @@
+var fs = require('fs');
+
+var j = fs.readFileSync("words.json").toString();
+var json = JSON.parse(j);
+
+function ToLanna(content){
+    lanna = content;
+    //ME
+    Object.keys(json.me).forEach(function(e){
+        lanna = lanna.replace(e,json.me[e]);
+    });
+
+    //YOU
+    Object.keys(json.you).forEach(function(e){
+        lanna = lanna.replace(e,json.you[e]);
+    });
+
+    //TIMES
+    Object.keys(json.times).forEach(function(e){
+        lanna = lanna.replace(e,json.times[e]);
+    });
+
+    //OTHERS
+    Object.keys(json.others).forEach(function(e){
+        lanna = lanna.replace(e,json.others[e]);
+    });
+
+    //FAMILY
+    Object.keys(json.family).forEach(function(e){
+        lanna = lanna.replace(e,json.family[e]);
+    });
+
+    //NUMBERS
+    Object.keys(json.numbers).forEach(function(e){
+        lanna = lanna.replace(e,json.numbers[e]);
+    });
+    return lanna;
+}
+
+
+ToLanna("เราเป็นชาวเหนือ พูดภาษาเหนือได้, คุณล่ะ พูดได้มั้ย");
